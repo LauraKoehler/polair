9fa9d2652ebef6a6c5a9d37f0ce69f337844647f
Johannes Röttenbacher <jr-alias@posteo.de>
2026-07-30 13:55:18 +0200

    Adapt docstrings to Google Format (_helpers.py)
    - includes new build

--- polair/_helpers.py
+++ polair/_helpers.py
@@ -1,7 +1,7 @@
 """
 Functions for DMS data processing
 
-Part of th epolair package.
+Part of the polair package.
 
 | title: _helpers.py
 | author: Laura Köhler
@@ -22,19 +22,18 @@ import os
 
 def import_dictionary(yaml_file):
     """
-    Import config file for respective campaign and other dictionaries
-    
+    Import config file for respective campaign and other dictionaries.
+
     The config file contains:
     - campaign information
     - Paths to xml-file, data
 
-    Parameters:
-    - yaml_file: .yaml file 
-        Dictionary containing basic information about the campaign
+    Args:
+        yaml_file: .yaml file
+            Dictionary containing basic information about the campaign
 
     Returns:
-    - dict
-        The config file as dictionary
+        dict: The config file as dictionary
     """
     try:
         with open(yaml_file, "r") as f:
@@ -45,29 +44,29 @@ def import_dictionary(yaml_file):
 
 def add2logfile(logfile, text):
     """
-    Writes text in the logfile
+    Writes text in the logfile.
 
-    Parameters:
-    - logfile: .txt file
-        Logfile for the processing, location defined in the config file
-    - text: str
-        Text which should be added to the logfile
+    Args:
+        logfile: .txt file
+            Logfile for the processing, location defined in the config file
+        text: str
+            Text which should be added to the logfile
     """
     with logfile.open("a") as f:
         f.write(text+"\n")
 
 def create_logfile(config):
     """
-    Check if processing log file already exists, if not, create it, print date and time
+    Check if processing log file already exists, if not, create it, print date and time.
+
     The config file needs to contain the path information of the log file.
 
-    Paramters:
-    - config: dict
-        Configuration dictionary
+    Args:
+        config: dict
+            Configuration dictionary
 
     Returns:
-    - logfile: str
-        Path to logfile
+        str: Path to logfile
     """
     fn = config["paths"]["processing_log_file"]+"turbulence_processing_log.txt"
     logfile = Path(fn)
@@ -82,19 +81,18 @@ def create_logfile(config):
 def get_variable_names(xml_file):
     """
     Read a DMS order XML file and extract the variable names created by DMS.
-    
-    Background:
-    - DMS filenames depend on the device configuration during specific flights or campaigns.
-    - This function reads an XML order file (e.g., 'RAD_flightname.xml') and extracts
-      variable names based on <channel> tags that include 'deviceShortName' and 'channelShortName'.
 
-    Paramters:
-    - xml_file: str
-        Location of the xml file, should be included in the config
+    **Background:**
+
+    - DMS filenames depend on the device configuration during specific flights or campaigns.
+    - This function reads an XML order file (e.g., 'RAD_flightname.xml') and extracts variable names based on <channel> tags that include 'deviceShortName' and 'channelShortName'.
+
+    Args:
+        xml_file: str
+            Location of the xml file, should be included in the config
 
     Returns:
-    - variable_names: list
-        List with all variable names in the xml file.
+        list: List with all variable names in the xml file.
     """
     try:
         # Read the entire file as text
@@ -105,12 +103,12 @@ def get_variable_names(xml_file):
         # Keep only lines containing 'channelShortName'
         lines = [line for line in lines if 'channelShortName' in line]
         variable_names = []
-        
+
         # Extract device and channel short names from each line
         for line in lines:
             channel_match = re.search(r'channelShortName="([^"]+)"', line)
             device_match = re.search(r'deviceShortName="([^"]+)"', line)
-        
+
             if channel_match and device_match:
                 channel = channel_match.group(1)
                 device = device_match.group(1)
@@ -121,19 +119,18 @@ def get_variable_names(xml_file):
 
 def import_data(v, config, flight):
     """
-    Import file for single variable using path and prefix for the specific flight from config file
+    Import file for single variable using path and prefix for the specific flight from config file.
 
-    Parameters:
-    - v: str
-        Variable name
-    - config: dict
-        Configuration dictionary
-    - flight: int
-        Flight number
+    Args:
+        v: str
+            Variable name
+        config: dict
+            Configuration dictionary
+        flight: int
+            Flight number
 
     Returns:
-    - df: pandas.DataFrame
-        Dataframe with time and variable data.
+        pandas.DataFrame: Dataframe with time and variable data.
     """
     old_name = vars[v]["old"]
     fn = f"{config["flights"][flight]["data_dir"]}/{config["flights"][flight]["prefix"]}{old_name}.dat"
@@ -142,15 +139,14 @@ def import_data(v, config, flight):
 
 def get_timestamps(df):
     """
-    Repair timestamps to datetime64 datetimes
+    Repair timestamps to datetime64 datetimes.
 
-    Parameters:
-    - df: pandas.DataFrame
-        Dataframe with times and data imported from the DMS download.
+    Args:
+        df: pandas.DataFrame
+            Dataframe with times and data imported from the DMS download.
 
     Returns:
-    - df: pandas.DataFrame
-        Dataframe with the same data but datetime64 timestamps.
+        pandas.DataFrame: Dataframe with the same data but datetime64 timestamps.
     """
     dates_fixed = np.array([d.replace('/', '-') for d in df["date"]], dtype=object)
     times = (dates_fixed+"T" +df["time"].values).astype("datetime64[ns]")
@@ -160,17 +156,18 @@ def get_timestamps(df):
 def check_sampling(df, v, var_dict, logfile):
     """
     Check if sampling interval in df['time'] matches expected_sampling (seconds).
+
     95% of samples should match within tolerance. Identified gaps and sampling issues are written in the logfile.
 
-    Parameters:
-    - df: pandas.DataFrame
-        Dataframe with data
-    - v: str
-        Variable name
-    - var_dict: dict
-        Dictionary with varible information
-    - logfile: str
-        Logfile path
+    Args:
+        df: pandas.DataFrame
+            Dataframe with data
+        v: str
+            Variable name
+        var_dict: dict
+            Dictionary with varible information
+        logfile: str
+            Logfile path
     """
     expected_sampling = var_dict[v]["raw_sampling_time"]
     dt = df["time"].diff().dt.total_seconds().iloc[1:]
@@ -195,20 +192,19 @@ def check_sampling(df, v, var_dict, logfile):
 def find_gaps(df, v, var_dict, logfile, gap_factor=2.0):
     """
     Find data gaps in a time series based on sampling interval.
-    
-    Parameters
-    - df : pandas.DataFrame
-        Must contain a 'time' column of datetime64.
-    - v: str
-        Variable of the dataframe.
-    - var_dict : dictionary
-        Inlcudes raw_sampling_time for each variable.
-    - gap_factor : float, optional
-        Threshold factor (default 2.0).
-    
-    Returns
-    - pandas.DataFrame
-        Each row is a gap interval: [start_time, end_time, gap_duration].
+
+    Args:
+        df: pandas.DataFrame
+            Must contain a 'time' column of datetime64.
+        v: str
+            Variable of the dataframe.
+        var_dict: dict
+            Inlcudes raw_sampling_time for each variable.
+        gap_factor: float, optional
+            Threshold factor (default 2.0).
+
+    Returns:
+        pandas.DataFrame: Each row is a gap interval: [start_time, end_time, gap_duration].
     """
     expected_sampling = var_dict[v]["raw_sampling_time"]
     # Compute time differences in seconds
@@ -231,26 +227,25 @@ def find_gaps(df, v, var_dict, logfile, gap_factor=2.0):
         add2logfile(logfile, f"{v}: gaps")
         for i in np.arange(len(gaps_df)):
             add2logfile(logfile, f"{i}: period: {gaps_df.iloc[0]["start_time"]} - {gaps_df.iloc[0]["end_time"]}, duration: {gaps_df.iloc[0]["gap_duration_s"]} s")
-        
+
     return gaps_df
 
 def interpolate_time(df, v, var_dict, steps = 0.01):
     """
     Linearly interpolate time on common timestamps with 100 Hz (or choose accordingly), convert to xarray dataset.
 
-    Paramters:
-    - df: pandas.DataFrame
-        Dataframe containing data.
-    - v: str
-        Variable name
-    - var_dict: dict
-        Dictionary containing variable information. It should be specified in the config file.
-    - steps: float
-        Optional, frequency specification given in s, default is 100 Hz, i.e. 0.01 s
+    Args:
+        df: pandas.DataFrame
+            Dataframe containing data.
+        v: str
+            Variable name
+        var_dict: dict
+            Dictionary containing variable information. It should be specified in the config file.
+        steps: float
+            Optional, frequency specification given in s, default is 100 Hz, i.e. 0.01 s
 
     Returns:
-    - ds: xarray.Dataset
-        Dataset interpolated to step frequency, default is 100 Hz.
+        xarray.Dataset: Dataset interpolated to step frequency, default is 100 Hz.
     """
     if type(df[v].values[-1]) == np.datetime64:
         ds = xr.Dataset(coords = {"time": df["time"]}, data_vars = {v: ("time", df[v].values)})
@@ -270,19 +265,20 @@ def interpolate_time(df, v, var_dict, steps = 0.01):
 
 def add_attrs_var(ds, v, var_dict):
     """
-    This function adds all attributes from the variable dictrionary except for the old name which is only necessary to read in the correct file. It adds the original unit because no unit conversion is done so far.
+    This function adds all attributes from the variable dictrionary except for the old name which is only necessary to read in the correct file.
 
-    Parameters:
-    - ds: xarray.Dataset
-        Input dataset
-    - v: str 
-        Variable for which attributes are added
-    - var_dict: dict
-        Dictionary with all the information for each parameter
+    It adds the original unit because no unit conversion is done so far.
+
+    Args:
+        ds: xarray.Dataset
+            Input dataset
+        v: str
+            Variable for which attributes are added
+        var_dict: dict
+            Dictionary with all the information for each parameter
 
     Returns:
-    - ds: xarray.Dataset
-        Dataset with added attributes.
+        xarray.Dataset: Dataset with added attributes.
     """
     attrs = var_dict[v]
     for at in list(attrs.keys()):
@@ -298,15 +294,15 @@ def add_attrs_var(ds, v, var_dict):
 
 def add_global_attrs(ds, config, flight):
     """
-    This function adds metadata to the data set as stated in the config file
+    This function adds metadata to the data set as stated in the config file.
 
-    Parameters:
-    - ds: xarray.Dataset
-        Input data set
-    - config: dict
-        config file containing campaign information
-    - flight: int
-        research flight which is processed
+    Args:
+        ds: xarray.Dataset
+            Input data set
+        config: dict
+            config file containing campaign information
+        flight: int
+            research flight which is processed
     """
     attrs = config["metadata"]
     ds.attrs = attrs
@@ -316,19 +312,18 @@ def add_global_attrs(ds, config, flight):
 
 def convert_unit(ds, var_dict, v):
     """
-    This function converts the unit from the original unit to the final (SI) unit specified in the variable information
+    This function converts the unit from the original unit to the final (SI) unit specified in the variable information.
 
-    Parameters:
-    - ds: xarray.Dataset
-        Input dataset
-    - var_dict: dict
-        Dictionary with variable (unit) information
-    - v: str
-        Variable for which unit should be converted
+    Args:
+        ds: xarray.Dataset
+            Input dataset
+        var_dict: dict
+            Dictionary with variable (unit) information
+        v: str
+            Variable for which unit should be converted
 
     Returns:
-    - ds: xarray.Dataset
-        Dataset with converted units
+        xarray.Dataset: Dataset with converted units
     """
     if not v in ["t_inat_gpgga", "t_inat_piahs", "t_gpgga", "t_gprmc"]:
         unit_old = str(var_dict[v]["units_old"])
@@ -342,41 +337,39 @@ def convert_unit(ds, var_dict, v):
     return ds
 
 def g_welmec(lat, h):
-    '''
-    ratio of gravitational acceleration according to Welmec-Formula devided by 9.81
+    """
+    Ratio of gravitational acceleration according to Welmec-Formula devided by 9.81.
 
-    Parameters:
-    - lat: xarray.DataArray
-        Latitude in degree
-    - h: xarray.DataArray
-        height above sea level in m
+    Args:
+        lat: xarray.DataArray
+            Latitude in degree
+        h: xarray.DataArray
+            height above sea level in m
 
     Returns:
-    - g_ratio: xarray.DataArray
-        Dataarray with latitude and height dependent values of g.
-    '''
+        xarray.DataArray: Dataarray with latitude and height dependent values of g.
+    """
     g_ratio = (9.780318 * (1 + 0.0053024 * np.sin(np.deg2rad(lat))**2 - 0.0000058 * np.sin(2*np.deg2rad(lat))**2) - 0.000003085 * h)
     return g_ratio
 
 def get_global_attributes(ds, config, instrument, flight):
-    '''
-    assigns attributes to the data set according to the config file.
+    """
+    Assigns attributes to the data set according to the config file.
 
-    Parameters:
-    - ds: xarray.Dataset
-        data set which should get attributes
-    - config: dict
-        configuration dictionary
-    - instrument: str
-        instrument as called in the condig file
-    - flight: int
-        flight number
+    Args:
+        ds: xarray.Dataset
+            data set which should get attributes
+        config: dict
+            configuration dictionary
+        instrument: str
+            instrument as called in the condig file
+        flight: int
+            flight number
 
     Returns:
-    - ds: xarray.Dataset
-        data set with attributes.
-    '''
-    try: 
+        xarray.Dataset: data set with attributes.
+    """
+    try:
         attributes = config["instrument_metadata"][instrument]
         ds.attrs = attributes
         ds.attrs["title"] = attributes["title"] + f"RF{flight:02} ({str(config["flights"][flight]["date"])})"
@@ -388,34 +381,33 @@ def get_global_attributes(ds, config, instrument, flight):
         return ds
 
 def add_segment_coordinate(ds, config, flight):
-    '''
-    assigns segment coordinate to a data set
+    """
+    Assigns segment coordinate to a data set.
 
-    Parameters:
-    - ds: xarray.Dataset
-        data set which is supposed to get the segment coordinate
-    - config: dict
-        configuration dictionary
-    - flight: in
-        flight number
+    Args:
+        ds: xarray.Dataset
+            data set which is supposed to get the segment coordinate
+        config: dict
+            configuration dictionary
+        flight: int
+            flight number
 
     Returns:
-    - ds: xarray.Dataset
-        data set with segment coordinate
-    '''
+        xarray.Dataset: data set with segment coordinate
+    """
     try:
         seg_fn = config["paths"]["segments"]
         segments = import_dictionary(seg_fn)
         flight_segments = segments["flights"][flight]["segments"]
-        
+
         times  = [np.datetime64(s["time"]) for s in flight_segments]
         labels = [s["segment"] for s in flight_segments]
-        
+
         # For each timestep, find which segment it belongs to
         indices = np.searchsorted(times, ds.time.values, side="right") - 1
         indices = np.clip(indices, 0, len(labels) - 1)
         segment_list = np.array(labels)[indices]
-        
+
         ds = ds.assign_coords(segment=("time", segment_list))
         ds["segment"].attrs = {"long_name": "segment of the research flight"}
         return ds
@@ -424,21 +416,20 @@ def add_segment_coordinate(ds, config, flight):
         return ds
 
 def import_device_data(indir, dev, time_offset):
-    '''
-    import data from different devices
+    """
+    Import data from different devices.
 
-    Parameters:
-    - indir: str
-        input directory
-    - dev: str
-        device
-    - time_offset: int
-        offset time in ms between device and noseboom to be defined in config file
+    Args:
+        indir: str
+            input directory
+        dev: str
+            device
+        time_offset: int
+            offset time in ms between device and noseboom to be defined in config file
 
     Returns:
-    - ds: xarray.Dataset
-        combined data from files in input directory
-    '''
+        xarray.Dataset: combined data from files in input directory
+    """
     if dev  == "mcpc":
         files = np.sort([f for f in os.listdir(indir) if f.endswith('.TXT')])
     elif dev == "partector":
@@ -447,7 +438,7 @@ def import_device_data(indir, dev, time_offset):
         files = np.sort([f for f in os.listdir(indir) if f.endswith('.dat')])
     elif dev == "kt19":
         files = np.sort([f for f in os.listdir(indir) if f.endswith('KT19serial.ERR.dat')])
-        
+
     for fn in files:
         if dev == "mcpc":
             df = pd.read_csv(f"{indir}/{fn}", header = 13, sep = "\t")
@@ -480,40 +471,39 @@ def import_device_data(indir, dev, time_offset):
     return ds
 
 def resample2sec(ds, resample, freq = "1s"):
-    '''
-    resample data to 1s (default) temporal resolution. e.g. if timestamps from DMS. Used method is 'nearest'. This function is optional and not used per default
+    """
+    Resample data to 1s (default) temporal resolution.
 
-    Parameters:
-    - ds: xarray.Dataset
-        data to be resampled
-    - resample: bool
-        resampling is only done if resample is put to True
-    - freq: str
-        resampling frequency. Default is "1s" which corresponds to 1 second
+    e.g. if timestamps from DMS. Used method is 'nearest'. This function is optional and not used per default.
+
+    Args:
+        ds: xarray.Dataset
+            data to be resampled
+        resample: bool
+            resampling is only done if resample is put to True
+        freq: str
+            resampling frequency. Default is "1s" which corresponds to 1 second
 
     Returns:
-    - ds: xarray.Dataset
-        resampled dataset if sampling is put to True. Else ds is not changed
-
-    '''
+        xarray.Dataset: resampled dataset if sampling is put to True. Else ds is not changed
+    """
     if resample:
         ds = ds.resample(time = freq).nearest()
     return ds
 
 def import_radiation_data(fn, name):
-    '''
-    imports the radiation file from the DMS raw data
+    """
+    Imports the radiation file from the DMS raw data.
 
-    Parameters:
-    - fn: str
-        filename
-    - name: str
-        variable name
+    Args:
+        fn: str
+            filename
+        name: str
+            variable name
 
     Returns:
-    - ds: xarray.Dataset
-        data set with variable data
-    '''
+        xarray.Dataset: data set with variable data
+    """
     data = pd.read_csv(fn, header  = 4, sep = r'\s+', names = ["date", "times", name])
     times = pd.to_datetime(data["date"].values+"T"+data["times"].values)
     data["time"] = times
